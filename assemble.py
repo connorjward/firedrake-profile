2e0858802bc8ab73de933071ced7d5a92f0a37f4
Connor Ward <c.ward20@imperial.ac.uk>
2020-11-06 11:18:49 +0000

    Add Problem class

--- assemble.py
+++ assemble.py
@@ -1,44 +1,32 @@
 import matplotlib.pyplot as plt
+import pyop2.profiling
 from firedrake import *
 
-import forms
-import meshes
+from problem import Problem
 
 
 MESH_SIZE = 10
-MESH_TYPE = "tri"
+MESH_TYPE = "quad"
 
-FORM_TYPE = "mass"
+FORM_TYPE = "helmholtz"
 DEGREE = 1
 
 N_WARM_START_REPEATS = 1
 N_REPEATS = 5
 
 
-# create the mesh
-if MESH_TYPE == "tri":
-    mesh = meshes.tri(MESH_SIZE)
-elif MESH_TYPE == "quad":
-    mesh = meshes.quad(MESH_SIZE)
-elif MESH_TYPE == "tet":
-    mesh = meshes.tet(MESH_SIZE)
-elif MESH_TYPE == "hex":
-    mesh = meshes.hex(MESH_SIZE)
-else:
-    raise AssertionError
-
-# generate the form
-if FORM_TYPE == "mass":
-    form = forms.mass(mesh, DEGREE)
-elif FORM_TYPE == "helmholtz":
-    form = forms.helmholtz(mesh, DEGREE)
-else:
-    raise AssertionError
+problem = Problem(MESH_TYPE, MESH_SIZE, DEGREE, FORM_TYPE)
 
 # do warm starts
 for _ in range(N_WARM_START_REPEATS):
-    out = assemble(form)
+    out = assemble(problem.form)
 
+PETSc.Log.begin()
 # do main run
 for _ in range(N_REPEATS):
-    assemble(form, tensor=out)
+    with PETSc.Log.Stage("assemble"):
+        assemble(problem.form, tensor=out)
+
+# postprocessing
+#PETSc.Log.view()
+print("DoF: {}".format(problem.function_space.dof_count))
