377c18fa25fd6edc19b4cb3e105e9d7e891d3103
Connor Ward <c.ward20@imperial.ac.uk>
2020-11-09 09:53:46 +0000

    Save PETSc log to a file

--- assemble.py
+++ assemble.py
@@ -5,12 +5,14 @@ Script that assembles a matrix.
 import argparse
 
 from firedrake import *
+from firedrake.petsc import OptionsManager
 
 from form import make_form
 from mesh import make_mesh
 
 
 parser = argparse.ArgumentParser()
+parser.add_argument("-o", dest="fout", default="assemble.log", type=str)
 parser.add_argument("--form", default="helmholtz", type=str)
 parser.add_argument("--mesh", default="tri", type=str,
                     choices=["tri", "quad", "tet", "hex"])
@@ -19,21 +21,29 @@ parser.add_argument("--degree", default=1, type=int)
 parser.add_argument("--repeats", default=1, type=int)
 args = parser.parse_args()
 
-mesh = make_mesh(args.mesh, args.mesh_size)
-V = FunctionSpace(mesh, "CG", degree=args.degree)
-
-form = make_form(args.form, V)
-
-# Do a warm start and save the resulting tensor to prevent reallocation 
-# in future.
-out = assemble(form)
-
-# Do main run.
+# Enable PETSc logging.
 PETSc.Log.begin()
 
+with PETSc.Log.Stage("Make mesh"):
+    mesh = make_mesh(args.mesh, args.mesh_size)
+
+with PETSc.Log.Stage("Make function space"):
+    V = FunctionSpace(mesh, "CG", degree=args.degree)
+
+with PETSc.Log.Stage("Make form"):
+    form = make_form(args.form, V)
+
+# Do a warm start and save the resulting tensor to prevent reallocation
+# in future.
+with PETSc.Log.Stage("Assemble (warm start)"):
+    out = assemble(form)
+
+# Do main run.
 for _ in range(args.repeats):
-    with PETSc.Log.Stage("assemble"):
+    with PETSc.Log.Stage("Assemble"):
         assemble(form, tensor=out)
 
+
 # postprocessing
-PETSc.Log.view()
+print("# cells: {}, DoF: {}".format(mesh.num_cells(), V.dof_count))
+PETSc.Log.view(PETSc.Viewer.ASCII(args.fout))
