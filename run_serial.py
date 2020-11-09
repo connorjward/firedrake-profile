db8b717ab70cf07a0fc4011c964646dc8587ac63
Connor Ward <c.ward20@imperial.ac.uk>
2020-11-09 15:41:53 +0000

    Add timestamp output dir

--- run_serial.py
+++ run_serial.py
@@ -1,14 +1,14 @@
 """
 """
 
-import glob
+import datetime
 import os
 import subprocess
 
 import numpy as np
 
 
-DATA_DIR = "/home/connor/data/firedrake-profile"
+DATA_ROOT_DIR = "/home/connor/data/firedrake-profile"
 
 #FORM_TYPES = ["mass", "helmholtz"]
 #MESH_TYPES = ["tri", "quad", "tet", "hex"]
@@ -34,11 +34,10 @@ def get_mesh_sizes(mesh_type):
         return np.linspace(20, 30, 3, dtype=np.int)
 
 
-DATA_DIR += "/"
-
-# Clear the output directory.
-for filename in glob.glob(DATA_DIR + "*"):
-    os.remove(filename)
+# Store the results in a time-stamped directory.
+data_dir = (DATA_ROOT_DIR + "/" 
+            + datetime.datetime.now().isoformat(timespec="seconds") + "/")
+os.mkdir(data_dir)
 
 # Run the tests.
 for form_type in FORM_TYPES:
@@ -51,8 +50,8 @@ for form_type in FORM_TYPES:
                 meta_fname = "metadata.csv"
 
                 cmd = ["python", "assemble.py",
-                       "-o", DATA_DIR + log_fname,
-                       "--meta", DATA_DIR + meta_fname,
+                       "-o", data_dir + log_fname,
+                       "--meta", data_dir + meta_fname,
                        "--form", form_type,
                        "--mesh", mesh_type,
                        "--mesh-size", str(mesh_size),
