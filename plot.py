e6b0ed38d8be794701edf8ff2f020e20e428963c
Connor Ward <c.ward20@imperial.ac.uk>
2020-11-09 16:10:08 +0000

    Loop over forms and meshes

--- plot.py
+++ plot.py
@@ -1,20 +1,46 @@
+"""
+???
+"""
+
+import argparse
+
 import matplotlib.pyplot as plt
 import pandas as pd
 
 import logparser
 
 
-META_FNAME = "/home/connor/data/firedrake-profile/metadata.csv"
+FORM_TYPES = ["mass", "helmholtz"]
+MESH_TYPES = ["tri", "quad", "tet", "hex"]
+DEGREES = range(1, 3)
 
-df = pd.read_csv(META_FNAME)
+parser = argparse.ArgumentParser()
+parser.add_argument("metadata_file", type=str, 
+                    help="Location of the metadata file.")
+parser.add_argument("--output-dir", default=".", type=str)
+args = parser.parse_args()
 
-times = []
-for filename in df.filename:
-    with open(filename) as f:
-        log = f.read()
-    stages = logparser.parse_stages(log)
-    times.append(stages["Assemble"].group("time"))
+df = pd.read_csv(args.metadata_file)
 
-plt.plot(df.dof, times)
-plt.scatter(df.dof, times, color="k", marker="x")
-plt.savefig("figure")
+for form_type in FORM_TYPES:
+    for mesh_type in MESH_TYPES:
+        for degree in DEGREES:
+            # Filter the data.
+            filtered_df = df[(df.form == form_type) 
+                             & (df.mesh == mesh_type) 
+                             & (df.degree == degree)]
+
+            times = []
+            for filename in filtered_df.filename:
+                with open(filename) as f:
+                    log = f.read()
+                stages = logparser.parse_stages(log)
+                times.append(stages["Assemble"].group("time"))
+
+            fig, ax = plt.subplots()
+            ax.plot(filtered_df.dof, times)
+            ax.scatter(filtered_df.dof, times, color="k", marker="x")
+            fig.tight_layout()
+            plt.savefig("{dir}/{form_type}_{mesh_type}_{degree}.png"
+                        .format(dir=args.output_dir, form_type=form_type, 
+                                mesh_type=mesh_type, degree=degree))
